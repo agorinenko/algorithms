36b5bbfb2478c2f0cb2c8a47f6b8c4f0c912409a
anton <anton.gorinenko@gmail.com>
2020-10-09 09:43:27 +0300

    Сортировка вставкой

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -4,7 +4,20 @@ from typing import List
 class InsertionSort:
     """
     Сортировка вставкой
+    Сложность  - O(n^2)
+    Массив разбивается на отсортированную последовательность слева и то, что надо отсортировать справа
     """
 
-    def sort(self, sequence: List[int])->List[int]:
-        pass
+    def sort(self, sequence: List[int], reverse: bool = False) -> List[int]:
+        j = 1
+        for key in sequence[j:]:
+            i = j - 1
+            # вставляем  sequence[j] в отсортированную последовательность слева
+            while i >= 0 and (sequence[i] < key if reverse else sequence[i] > key):
+                sequence[i + 1] = sequence[i]
+                i -= 1
+
+            sequence[i + 1] = key
+            j += 1
+
+        return sequence
--- sort/test_insertion_sort.py
+++ sort/test_insertion_sort.py
@@ -16,3 +16,42 @@ class DefaultTestCase(unittest.TestCase):
         self.assertEqual(b[3], 41)
         self.assertEqual(b[4], 58)
         self.assertEqual(b[5], 59)
+
+    def test_main_1(self):
+        a = [1, 2, 3, 4, 5, 6]
+
+        obj = InsertionSort()
+
+        b = obj.sort(a)
+        self.assertEqual(b[0], 1)
+        self.assertEqual(b[1], 2)
+        self.assertEqual(b[2], 3)
+        self.assertEqual(b[3], 4)
+        self.assertEqual(b[4], 5)
+        self.assertEqual(b[5], 6)
+
+    def test_main_2(self):
+        a = [6, 5, 4, 3, 2, 1]
+
+        obj = InsertionSort()
+
+        b = obj.sort(a)
+        self.assertEqual(b[0], 1)
+        self.assertEqual(b[1], 2)
+        self.assertEqual(b[2], 3)
+        self.assertEqual(b[3], 4)
+        self.assertEqual(b[4], 5)
+        self.assertEqual(b[5], 6)
+
+    def test_main_3(self):
+        a = [1, 2, 3, 4, 5, 6]
+
+        obj = InsertionSort()
+
+        b = obj.sort(a, reverse=True)
+        self.assertEqual(b[0], 6)
+        self.assertEqual(b[1], 5)
+        self.assertEqual(b[2], 4)
+        self.assertEqual(b[3], 3)
+        self.assertEqual(b[4], 2)
+        self.assertEqual(b[5], 1)
