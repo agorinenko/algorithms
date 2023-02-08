eab7aa8bba4051938c1bd95fbc81d49d5bcadce1
Anton Gorinenko <gorinenko@vtb.ru>
2023-02-08 12:16:45 +0300

    matrix1

--- leet_code/linked_list/my_linked_list.py
+++ leet_code/linked_list/my_linked_list.py
@@ -1,28 +1,32 @@
+"""
+DoublyLinkedList
+"""
+from dataclasses import dataclass
 from typing import Optional
 
 
+@dataclass
 class DoublyListNode:
     """
     Узел двусвязного списка
     """
-
-    def __init__(self, value):
-        self.val = value
-        self.prev = None
-        self.next = None
+    val: int
+    prev: Optional['DoublyListNode'] = None
+    next: Optional['DoublyListNode'] = None
 
 
-class MyLinkedList:
+class DoublyLinkedList:
+    """
+    Реализация двусвязного списка
+    """
+
     def __init__(self):
         self.head = None
         self.tail = None
+        self.len = 0
 
-    def get(self, index: int) -> int:
-        cur = self.get_node(index)
-
-        return cur.val if cur else -1
-
-    def get_node(self, index: int) -> Optional[DoublyListNode]:
+    def get(self, index: int) -> Optional[DoublyListNode]:
+        """ Получение узла по индексу """
         cur = self.head
         idx = 0
         while idx != index:
@@ -34,47 +38,106 @@ class MyLinkedList:
 
         return cur
 
-    def addAtHead(self, val: int) -> None:
-        node = DoublyListNode(val)
-        node.next = self.head
+    def add_at_head(self, val: int) -> DoublyListNode:
+        """ Добавление в голову """
+        cur = DoublyListNode(val)
+        cur.next = self.head
         if self.head:
-            self.head.prev = node
+            self.head.prev = cur
         if not self.tail:
-            self.tail = node
+            self.tail = cur
 
-        self.head = node
+        self.head = cur
+        self.len += 1
 
-    def addAtTail(self, val: int) -> None:
-        node = DoublyListNode(val)
-        node.prev = self.tail
+        return cur
+
+    def add_at_tail(self, val: int) -> DoublyListNode:
+        """ Добавление в хвост """
+        cur = DoublyListNode(val)
+        cur.prev = self.tail
         if self.tail:
-            self.tail.next = node
+            self.tail.next = cur
         if not self.head:
-            self.head = node
+            self.head = cur
 
-        self.tail = node
+        self.tail = cur
+        self.len += 1
 
-    def addAtIndex(self, index: int, val: int) -> None:
-        right = self.get_node(index)
+        return cur
+
+    def add_at_index(self, index: int, val: int) -> Optional[DoublyListNode]:
+        """ Добавление по индексу """
+        if index > self.len:
+            return None
+
+        right = self.get(index)
         if not right:
-            return self.addAtTail(val)
+            return self.add_at_tail(val)
 
         left = right.prev
         if not left:
-            return self.addAtHead(val)
+            return self.add_at_head(val)
 
         cur = DoublyListNode(val)
         cur.prev = left
         cur.next = right
         left.next = cur
         right.prev = cur
+        self.len += 1
+        return cur
 
-    def deleteAtIndex(self, index: int) -> None:
-        curr = self.get_node(index)
+    def delete_at_index(self, index: int) -> None:
+        """ Удаление по индексу """
+        curr = self.get(index)
         if not curr:
             return None
 
-        right = curr.next
         left = curr.prev
-        left.next  = right
+        right = curr.next
+
+        if not left:
+            return self.delete_at_head()
+
+        if not right:
+            return self.delete_at_tail()
+
         right.prev = left
+        left.next = right
+        self.len -= 1
+
+        return None
+
+    def delete_at_head(self) -> None:
+        """ Удаление из головы """
+        if not self.head:
+            return None
+
+        cur = self.head.next
+        if cur:
+            cur.prev = None
+        else:
+            # Если в списке один элемент и мы его удаляем
+            self.tail = None
+
+        self.head = cur
+        self.len -= 1
+
+        return None
+
+    def delete_at_tail(self) -> None:
+        """ Удаление с хвоста """
+        if not self.tail:
+            return None
+
+        cur = self.tail.prev
+        if cur:
+            cur.next = None
+        else:
+            # Если в списке один элемент и мы его удаляем
+            self.head = None
+
+        self.tail = cur
+        self.len -= 1
+
+        return None
--- leet_code/linked_list/tests/test_my_linked_list.py
+++ leet_code/linked_list/tests/test_my_linked_list.py
@@ -1,12 +1,12 @@
-from leet_code.linked_list.my_linked_list import MyLinkedList
+from leet_code.linked_list.my_linked_list import DoublyLinkedList
 
 
 def test_main_1():
-    my_list = MyLinkedList()
-    my_list.addAtHead(1)
-    my_list.addAtTail(3)
-    my_list.addAtIndex(1, 2)
-    my_list.get(1)
-    my_list.deleteAtIndex(1)
+    my_list = DoublyLinkedList()
+    my_list.add_at_head(1)
+    my_list.add_at_tail(3)
+    my_list.add_at_index(1, 2)  # linked list becomes 1->2->3
+    assert 2 == my_list.get(1).val  # return 2
+    my_list.delete_at_index(1)  # now the linked list is 1->3
 
-    my_list.get(1)
+    assert 3 == my_list.get(1).val  # return 3
